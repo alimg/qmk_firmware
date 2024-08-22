2f5ce604795ada61f80a3b8f927165c6e1d96c8f
Alim Gokkaya <alim.gokkaya@gmail.com>
2024-08-22 01:43:18 +0100

    K3v2 bring up (RGB Optical only)
    - BT is disconnects after pressing a key
    - dip switch pins might be still incorrect (win/mac layers seem to work, but there is actually a 3rd layer on the device for iOS)
    - led brighness setting just makes lights flicker

---
+++ keyboards/keychron/k3/rgb/v2/keymaps/via_iso/keymap.h
@@ -0,0 +1,39 @@
+/*
+Copyright 2020 Dimitris Mantzouranis
+Copyright 2022 Pablo Ramirez
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+enum BT_keycodes { BT_PROFILE1 = SAFE_RANGE, BT_PROFILE2, BT_PROFILE3, BT_PAIR, BT_TOGGLE, BT_RESET, BT_BATTERY, K3_SAFE_RANGE };
+enum layer_names {
+  MAC_BASE = 0,
+  WIN_BASE,
+  MAC_FN,
+  WIN_FN
+};
+
+#define KC_TASK LGUI(KC_TAB)        // Task viewer
+#define KC_FLXP LGUI(KC_E)          // Windows file explorer
+#define KC_SNIP LGUI(LSFT(KC_S))    // Windows snip tool
+
+#define KC_MSSN LGUI(KC_F3)         // Mission Control
+#define KC_FIND LALT(LGUI(KC_SPC))  // Finder
+#define KC_MSCR LSFT(LGUI(KC_3))    // Mac screenshot
+#define KC_MSNP LSFT(LGUI(KC_4))    // Mac snip tool
